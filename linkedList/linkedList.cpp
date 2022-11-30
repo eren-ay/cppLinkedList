41bd45f7627e2ae3a2ecc9ab9bdbd4159a555e22
eren-ay <81091542+eren-ay@users.noreply.github.com>
2022-11-30 10:28:00 +0300

    Add files via upload

--- linkedList/linkedList.cpp
+++ linkedList/linkedList.cpp
@@ -18,7 +18,16 @@ public:
     void insert(T value);
     T pop();
     int lengthOfQueue();
+    T getLast();
+    T searcByIndex(int value);
 };
+
+
+
+
+
+
+
 
 template <typename T>
 queue<T>::queue()
@@ -54,30 +63,101 @@ void queue<T>::insert(T value)
     else
     {
         temp->next = headNode;
+        headNode->next=temp;
         headNode = temp;
     }
     length++;
 };
 
+
+
 template <typename T>
 T queue<T>::pop()
 {
-    if (lastNode == NULL)
+    
+    try
     {
-        printf("Error: Node is empty");
-        return lastNode->getValue();
+        if (lastNode == NULL){
+            string error="ertunun amk";
+            throw error;
+        }
     }
+    catch (string myNum)
+    {
+        cout << "\nError number: " << myNum;  
+    }
+    
     T temp = lastNode->getValue();
-    lastNode = lastNode->next();
+    lastNode = lastNode->next;
     return temp;
 }
+
+template <typename T>
+T queue<T>::getLast()
+{
+    
+    try
+    {
+        if (lastNode == NULL){
+            string error="ertunun amk";
+            throw error;
+        }
+    }
+    catch (string myNum)
+    {
+        cout << "\nError number: " << myNum;  
+    }
+    
+    T temp = headNode->getValue();
+    return temp;
+}
+
+template <typename T>
+T queue<T>::searcByIndex(int value)
+{
+    
+    try
+    {
+        if (lastNode == NULL){
+            string error="ertunun amk";
+            throw error;
+        }
+    }
+    catch (string myNum)
+    {
+        cout << "\nError number: " << myNum;  
+    }
+    Node<T> *tempNode = new Node<T>();
+    *tempNode=*lastNode;
+    for (size_t i = 0; i < value; i++)
+    {
+        *tempNode=*tempNode->next;
+    }
+    return tempNode->getValue();
+    
+}
+
+
+
+
+
+
 
 int main()
 {
     int sayi = 15;
     queue<int> *denemeStack = new queue<int>(sayi);
-    denemeStack->insert(11);
-    printf("%d", denemeStack->lengthOfQueue());
-    cout << denemeStack->pop();
+    denemeStack->insert(122);
+    denemeStack->insert(56);
+    denemeStack->insert(562);
+    denemeStack->insert(563);
+    denemeStack->insert(564);
+    int queueLength=denemeStack->lengthOfQueue();
+    //cout << "\n" <<  queueLength << endl;
+    //cout << "\n" << denemeStack->searcByIndex(0) << endl;
+    cout << "\n" << denemeStack->pop() << endl;
+    cout << "\n" << denemeStack->pop() << endl;
+
+
     return 0;
 };
